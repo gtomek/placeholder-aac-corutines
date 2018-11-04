0f3148159dab08fb9553f825bb0905a410125796
Tomasz Giszczak <tgiszczak@gmail.com>
2018-11-04 21:53:07 +0000

    Some changes in corutines implementation

--- app/src/main/java/uk/co/tomek/jsonplaceholderdemoapp/data/MainRepository.kt
+++ app/src/main/java/uk/co/tomek/jsonplaceholderdemoapp/data/MainRepository.kt
@@ -1,10 +1,6 @@
 package uk.co.tomek.jsonplaceholderdemoapp.data
 
 import androidx.lifecycle.MutableLiveData
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import uk.co.tomek.jsonplaceholderdemoapp.data.model.Comment
 import uk.co.tomek.jsonplaceholderdemoapp.data.model.Post
 import uk.co.tomek.jsonplaceholderdemoapp.data.model.User
@@ -17,40 +13,28 @@ import uk.co.tomek.jsonplaceholderdemoapp.data.network.NetworkService
  */
 class MainRepository(val networkService: NetworkService) {
 
-    // TODO: make them private
     val postsLiveData: MutableLiveData<List<Post>> = MutableLiveData()
     val commentsLiveData: MutableLiveData<List<Comment>> = MutableLiveData()
-    private val usersLiveData: MutableLiveData<List<User>> = MutableLiveData()
+    val usersLiveData: MutableLiveData<List<User>> = MutableLiveData()
 
-    fun fetchAll() {
+    suspend fun fetchAll() {
         fetchPosts()
         fetchComments()
+        fetchUsers()
     }
 
-    private fun fetchPosts() {
-        GlobalScope.launch {
-            val postsResult = networkService.getPosts().await()
-            withContext(Dispatchers.Main) {
-                postsLiveData.postValue(postsResult)
-            }
-        }
+    private suspend fun fetchPosts() {
+        val postsResult = networkService.getPosts().await()
+        postsLiveData.postValue(postsResult)
     }
 
-    private fun fetchComments() {
-        GlobalScope.launch {
-            val commentsResult = networkService.getComments().await()
-            withContext(Dispatchers.Main) {
-                commentsLiveData.postValue(commentsResult)
-            }
-        }
+    private suspend fun fetchComments() {
+        val commentsResult = networkService.getComments().await()
+        commentsLiveData.postValue(commentsResult)
     }
 
-    private fun fetchUsers() {
-        GlobalScope.launch {
-            val usersResult = networkService.getUsers().await()
-            withContext(Dispatchers.Main) {
-                usersLiveData.postValue(usersResult)
-            }
-        }
+    private suspend fun fetchUsers() {
+        val usersResult = networkService.getUsers().await()
+        usersLiveData.postValue(usersResult)
     }
 }
--- app/src/main/java/uk/co/tomek/jsonplaceholderdemoapp/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/uk/co/tomek/jsonplaceholderdemoapp/ui/viewmodel/MainViewModel.kt
@@ -3,37 +3,68 @@ package uk.co.tomek.jsonplaceholderdemoapp.ui.viewmodel
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import uk.co.tomek.jsonplaceholderdemoapp.data.MainRepository
 import uk.co.tomek.jsonplaceholderdemoapp.data.model.Comment
 import uk.co.tomek.jsonplaceholderdemoapp.data.model.Post
+import uk.co.tomek.jsonplaceholderdemoapp.data.model.User
 import uk.co.tomek.jsonplaceholderdemoapp.ui.viewstate.MainViewState
 
 /**
  * ViewModel for the main screen.
  */
-class MainViewModel(val mainRepository: MainRepository) : ViewModel() {
+class MainViewModel(private val mainRepository: MainRepository) : ViewModel() {
 
     private val mainLiveData: MediatorLiveData<MainViewState> = MediatorLiveData()
+
+    private lateinit var job: Job
 
     init {
         // set initial state
         mainLiveData.value = MainViewState.Loading
 
         mainLiveData.addSource(mainRepository.postsLiveData) {
-            mainLiveData.value = combineLatestData(mainRepository.postsLiveData, mainRepository.commentsLiveData)
+            mainLiveData.value = combineLatestData(
+                mainRepository.postsLiveData,
+                mainRepository.commentsLiveData,
+                mainRepository.usersLiveData
+            )
         }
         mainLiveData.addSource(mainRepository.commentsLiveData) {
-            mainLiveData.value = combineLatestData(mainRepository.postsLiveData, mainRepository.commentsLiveData)
+            mainLiveData.value = combineLatestData(
+                mainRepository.postsLiveData,
+                mainRepository.commentsLiveData,
+                mainRepository.usersLiveData
+            )
+        }
+        mainLiveData.addSource(mainRepository.usersLiveData) {
+            mainLiveData.value = combineLatestData(mainRepository.postsLiveData, mainRepository.commentsLiveData,
+                mainRepository.usersLiveData)
         }
     }
 
     fun getLiveData() = mainLiveData
 
     fun fetchData() {
-        mainRepository.fetchAll()
+        job = CoroutineScope(Dispatchers.IO).launch {
+            mainRepository.fetchAll()
+        }
     }
 
-    private fun combineLatestData(fetchPosts: MutableLiveData<List<Post>>, fetchComments: MutableLiveData<List<Comment>>): MainViewState {
+    private fun combineLatestData(
+        fetchPosts: MutableLiveData<List<Post>>,
+        fetchComments: MutableLiveData<List<Comment>>,
+        usersLiveData: MutableLiveData<List<User>>
+    ): MainViewState {
         return MainViewState.Data(fetchPosts.value)
     }
+
+    override fun onCleared() {
+        job.cancel()
+        super.onCleared()
+    }
+
 }
